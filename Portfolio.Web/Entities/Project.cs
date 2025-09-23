cbe73230a1a5db560a7d34c1559bcc96b29df5e9
Eren <Eren@EREN>
2025-09-23 11:53:33 +0300

    Proje dosyası ekle.

---
+++ Portfolio.Web/Entities/Project.cs
@@ -0,0 +1,14 @@
+﻿namespace Portfolio.Web.Entities
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+        public string GithubURL { get; set; }
+
+        //navigation property
+        public Category Category { get; set; }
+    }
+}
